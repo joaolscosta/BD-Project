329a4aa6ef1703289c3ba46e072fe4f38848519f
Guilherme Belchior <guilherme.belchior1@hotmail.com>
2023-05-23 14:39:30 +0100

    FIXED EVERYTHING

--- queries.sql
+++ queries.sql
@@ -15,42 +15,50 @@ WHERE
   p.price > 50;
 
 /* Exercicio 2 */
-SELECT DISTINCT
-  e.name
-From
-  Employee e
-INNER JOIN
-  process p ON e.ssn = p.ssn
-INNER JOIN
-  "order" o ON p.order_no = o.order_no
-WHERE
-  o.date > '2022-12-31'
-and
-  o.date < '2023-02-01'
 
-INTERSECT 
+CREATE VIEW EJAN2023 AS
+  SELECT DISTINCT
+    e.name
+  From
+    Employee e
+  INNER JOIN
+    process p ON e.ssn = p.ssn
+  INNER JOIN
+    "order" o ON p.order_no = o.order_no
+  WHERE
+    o.date > '2022-12-31'
+  and
+    o.date < '2023-02-01';
+
+CREATE VIEW EWHA AS 
+  SELECT DISTINCT 
+    e.name
+  FROM 
+    Employee e
+  WHERE e.ssn IN (
+    SELECT 
+      w.ssn
+    FROM 
+      works w
+    JOIN Warehouse wh ON w.address = wh.address
+  )
+  AND e.ssn NOT IN (
+    SELECT 
+      w.ssn
+    FROM 
+      works w
+    JOIN Office o ON w.address = o.address
+  );
 
 SELECT DISTINCT
-  e.name
-From
-  Employee e
-INNER JOIN
-  works wk ON e.ssn = wk.ssn
-INNER JOIN
-  (SELECT * 
+  name
+FROM
+  EJAN2023
+INTERSECT
+  SELECT DISTINCT
+    name
   FROM
-    Workplace wkp
-  Where
-    wkp.address NOT IN (
-      SELECT
-        o.address
-      FROM
-        Office o
-    )
-  ) wp ON wk.address = wp.address;
-
-INNER JOIN
-  Warehouse wa ON wp.address = wa.address
+    EWHA;
 
 /* Exercicio 3 */
 SELECT DISTINCT
@@ -68,3 +76,22 @@ WHERE
     FROM
       contains cs
 );
+
+/* Exercicio 4 */
+
+SELECT 
+  query.order_no, SUM(query.total)
+FROM
+  (
+    SELECT 
+      s.order_no, ct.quantity*p.price as total
+    FROM
+      Product p
+    INNER JOIN
+      contains ct ON p.sku = ct.sku
+    INNER JOIN
+      sale s ON s.order_no = ct.order_no
+  ) as query
+  GROUP BY
+  query.order_no;
+
