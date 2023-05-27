f29c7114f0bfa1aa934c0eed52a25d7dea7e5ee6
Guilherme Belchior <guilherme.belchior1@hotmail.com>
2023-05-27 15:33:23 +0100

    Fixed Some errors in queries.sql

--- queries.sql
+++ queries.sql
@@ -12,43 +12,51 @@ INNER JOIN
 INNER JOIN
   Product p ON cs.sku = p.sku
 WHERE
-  p.price > 50;
+  p.price > 50
+AND
+  o.date > '2022-12-31'
+AND
+  o.date < '2024-01-01';
 
 /* Exercicio 2 */
 
 WITH EJAN2023 AS
-  (SELECT DISTINCT
-    e.name
-  From
-    Employee e
-  INNER JOIN
-    process p ON e.ssn = p.ssn
-  INNER JOIN
-    "order" o ON p.order_no = o.order_no
-  WHERE
-    o.date > '2022-12-31'
-  and
-    o.date < '2023-02-01')
+  (
+    SELECT DISTINCT
+      e.name
+    From
+      Employee e
+    INNER JOIN
+      process p ON e.ssn = p.ssn
+    INNER JOIN
+      "order" o ON p.order_no = o.order_no
+    WHERE
+      o.date > '2022-12-31'
+    and
+      o.date < '2023-02-01'
+  ),
 
-WITH EWHA AS 
-  (SELECT DISTINCT 
-    e.name
-  FROM 
-    Employee e
-  WHERE e.ssn IN (
-    SELECT 
-      w.ssn
+EWHA AS 
+  (
+    SELECT DISTINCT 
+      e.name
     FROM 
-      works w
-    JOIN Warehouse wh ON w.address = wh.address
+      Employee e
+    WHERE e.ssn IN (
+      SELECT 
+        w.ssn
+      FROM 
+        works w
+      JOIN Warehouse wh ON w.address = wh.address
+    )
+    AND e.ssn NOT IN (
+      SELECT 
+        w.ssn
+      FROM 
+        works w
+      JOIN Office o ON w.address = o.address
+    )
   )
-  AND e.ssn NOT IN (
-    SELECT 
-      w.ssn
-    FROM 
-      works w
-    JOIN Office o ON w.address = o.address
-  ))
 
 SELECT DISTINCT
   name
@@ -67,6 +75,8 @@ FROM
   Product p
 INNER JOIN
   contains cs ON p.sku = cs.sku
+INNER JOIN
+  Sale s ON cs.order_no = s.order_no
 WHERE 
   cs.quantity = (
     SELECT
@@ -92,6 +102,6 @@ FROM
     INNER JOIN
       sale s ON s.order_no = ct.order_no
   ) as query
-  GROUP BY
+GROUP BY
   query.order_no;
 
